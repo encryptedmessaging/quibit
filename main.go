c433e93226edabfb08367aaa4774d7f9e30c740c
Alex Smith <aosmith@gmail.com>
2014-06-26 23:29:03 -0500

    started cleaning up and breaking out

--- main.go
+++ main.go
@@ -7,78 +7,95 @@ import (
   "reflect"
   "quibit"
   "crypto/sha512"
+  "errors"
 )
 
+
+var Log = make(chan string)
 
 func main() {
   listener, err := net.Listen("tcp", ":1337")
   if err != nil {
     fmt.Println(err.Error())
   }
-  log := make(chan string)
   go func() {
     for {
       conn, err := listener.Accept()
       if err != nil {
-        log <- err.Error()
+        Log <- err.Error()
       }
-      log <- "Connection Accepted!"
+      Log <- "Connection Accepted!"
       go func() {
         // So now we have a connection.  Let's shake hands.
-        log <- fmt.Sprintf("%v", conn)
-        var h quibit.Header
-        var headerBuffer bytes.Buffer
-        for {
-          headerSize := int(reflect.TypeOf(h).Size())
-          log <- fmt.Sprintf("Header size: %d", headerSize)
-          buffer := make([]byte, headerSize)
-          n, err := conn.Read(buffer)
-          if err != nil {
-            if err.Error() == "EOF" {
-              break
-            }
-            log <- err.Error()
-          }
-          if n > 0 {
-            fmt.Println(buffer)
-            headerBuffer.Write(buffer)
-            log <- fmt.Sprintf("%b", headerBuffer.Bytes())
-            if len(headerBuffer.Bytes()) == headerSize {
-              h.FromBytes(headerBuffer.Bytes())
-              log <- fmt.Sprintf("%d", h.Magic)
-              log <- fmt.Sprintf("%d", h.Length)
-              var payloadBuffer bytes.Buffer
-              if h.Length > 0 {
-                for {
-                  log <- fmt.Sprintf("Payload size: %d", h.Length)
-                  buffer := make([]byte, h.Length)
-                  n, err := conn.Read(buffer)
-                  if err != nil {
-                    log <- err.Error()
-                  }
-                  if n > 0 {
-                    payloadBuffer.Write(buffer)
-                    if len(payloadBuffer.Bytes()) == int(h.Length) {
-                      //Checksum
-                      if h.Checksum != sha512.Sum384(payloadBuffer.Bytes()) {
-                        log <- "Incorrect Checksum!"
-                      }
-                      log <- fmt.Sprintf("Payload: %s", payloadBuffer.Bytes())
-                      break
-                    }
-                  }
-                }
-              }
-            }
-          }
+        Log <- fmt.Sprintf("%v", conn)
+        header := RecvHeader(conn)
+        payload, err := RecvPayload(conn, header)
+        if err != nil {
+          Log <- fmt.Sprint("%s", err.Error())
         }
+        Log <- fmt.Sprintf("%s", payload)
       }()
     }
   }()
   for {
-    message := <- log
+    message := <- Log
     fmt.Println(message)
   }
 }
 
+func RecvHeader(conn net.Conn) quibit.Header {
+  var h quibit.Header
+  var headerBuffer bytes.Buffer
+  for {
+    headerSize := int(reflect.TypeOf(h).Size())
+    Log <- fmt.Sprintf("Header size: %d", headerSize)
+    buffer := make([]byte, headerSize)
+    n, err := conn.Read(buffer)
+    if err != nil {
+      if err.Error() == "EOF" {
+        break
+      }
+      Log <- err.Error()
+    }
+    if n > 0 {
+      fmt.Println(buffer)
+      headerBuffer.Write(buffer)
+      Log <- fmt.Sprintf("%b", headerBuffer.Bytes())
+      if len(headerBuffer.Bytes()) == headerSize {
+        h.FromBytes(headerBuffer.Bytes())
+        Log <- fmt.Sprintf("%d", h.Magic)
+        Log <- fmt.Sprintf("%d", h.Length)
+        return h
+      }
+    }
+  }
+  // Should never end here
+  panic("RECV HEADER")
+  return h
+}
 
+func RecvPayload(conn net.Conn, h quibit.Header) ([]byte, error) {
+  payload := make([]byte, h.Length)
+  var payloadBuffer bytes.Buffer
+  if h.Length < 1 {
+    return payload, errors.New("Length < 1")
+  }
+  for {
+    n, err := conn.Read(payload)
+    if err != nil {
+      return payload, err
+    }
+    if n > 1 {
+      payloadBuffer.Write(payload)
+      if len(payloadBuffer.Bytes()) == int(h.Length) {
+        if h.Checksum != sha512.Sum384(payloadBuffer.Bytes()) {
+          return payloadBuffer.Bytes(), errors.New("Incorrect Checksum")
+        }
+        return payloadBuffer.Bytes(), nil
+      }
+    }
+  }
+  //Should never end here
+  panic("RECV PAYLOAD")
+  return payload, nil
+}
