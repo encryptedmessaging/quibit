9dd6cfe621912bc414ce6a6aeca47363a077eb54
Ethan Gordon <ekgordon2@gmail.com>
2014-08-21 10:19:45 -0500

    Added BSD 3-Clause license

--- quibit_test.go
+++ quibit_test.go
@@ -1,3 +1,14 @@
+/**
+    Copyright 2014 JARST, LLC
+    
+    This file is part of Quibit.
+
+    Quibit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    LICENSE file for details.
+**/
+
 package quibit
 
 import (
